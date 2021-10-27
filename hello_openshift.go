0033c605b0d98b88f3eb13b0a40b6897b15a263f
Dmirtii Zhdanov <dvzhdanov@vtb.ru>
2021-10-27 14:29:21 +0300

    Add prometheus

--- hello_openshift.go
+++ hello_openshift.go
@@ -1,42 +1,13 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
-	"os"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	response := os.Getenv("RESPONSE")
-	if len(response) == 0 {
-		response = "Hello OpenShift!"
-	}
-
-	fmt.Fprintln(w, response)
-	fmt.Println("Servicing request.")
-}
-
-func listenAndServe(port string) {
-	fmt.Printf("serving on %s\n", port)
-	err := http.ListenAndServe(":"+port, nil)
-	if err != nil {
-		panic("ListenAndServe: " + err.Error())
-	}
-}
-
 func main() {
-	http.HandleFunc("/", helloHandler)
-	port := os.Getenv("PORT")
-	if len(port) == 0 {
-		port = "8080"
-	}
-	go listenAndServe(port)
-
-	port = os.Getenv("SECOND_PORT")
-	if len(port) == 0 {
-		port = "8888"
-	}
-	go listenAndServe(port)
-
-	select {}
+	http.Handle("/metrics", promhttp.Handler())
+	log.Fatal(http.ListenAndServe(":9101", nil))
 }
